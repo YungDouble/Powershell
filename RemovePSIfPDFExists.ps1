d816085636b9306845e755840325c6914b99d388
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2022-06-28 12:41:59 -0500

    ps file to remove ps files if a pdf file exists

---
+++ RemovePSIfPDFExists.ps1
@@ -0,0 +1,8 @@
+﻿Get-ChildItem *.pdf -Recurse | Where-Object {
+  -not $_.PSIsContainer
+} | ForEach-Object {
+  $txtfile = Join-Path $_.Directory ($_.BaseName + '.ps')
+  if (Test-Path -LiteralPath $txtfile) {
+    Remove-Item $txtfile -Force
+  }
+}
