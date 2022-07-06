a4c974455c345987e596c52eca63bf799f980192
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2022-07-06 13:31:28 -0500

    add comments

--- RemovePSIfPDFExists.ps1
+++ RemovePSIfPDFExists.ps1
@@ -1,4 +1,6 @@
-﻿Get-ChildItem *.pdf -Recurse | Where-Object {
+<#This script will go through folders and if there is a .ps file AND a matching file with another extension, delte the .ps file #>
+
+Get-ChildItem *.pdf -Recurse | Where-Object {
   -not $_.PSIsContainer
 } | ForEach-Object {
   $txtfile = Join-Path $_.Directory ($_.BaseName + '.ps')
