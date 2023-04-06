b792b83fe0d6a4948eec003f0dd819c0932a848d
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2023-04-05 21:05:09 -0500

    initial commit

---
+++ editMultipleCSV_inSubfolders.ps1
@@ -0,0 +1,18 @@
+$rootPath = "./"
+
+# Get all CSV files in the root folder and its subfolders
+Get-ChildItem -Path $rootPath -Recurse -Include *.csv | ForEach-Object {
+    # Import the CSV file
+    $csv = Import-Csv $_.FullName
+
+    # Modify the CSV data as needed
+    $csv | ForEach-Object {
+ 				$_.DocSuperCategory = "Miscellaneous"
+                $_.DocCategory = "Miscellaneous"
+                $_.DocType = "Miscellaneous"
+                $_.DateofBirth = "1/1/1753"
+    }
+
+    # Export the modified CSV data to the same file
+    $csv | Export-Csv $_.FullName -NoTypeInformation
+}
