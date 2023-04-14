e069ccf06d7f073e25378a0382e9e5c3c2bd5f59
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2023-04-14 16:34:21 -0500

    initial commit

---
+++ clearToMark_multipleBatches.ps1
@@ -0,0 +1,19 @@
+$searchStringStart = "mark currentfile"
+$searchStringEnd = "cleartomark"
+$replacementString = ""
+
+# Get all .ps files in the directory
+$files = Get-ChildItem -Path "./" -Recurse -Filter "*.ps"
+
+foreach ($file in $files) {
+$content = Get-Content $file.FullName -Raw
+$startIndex = $content.IndexOf($searchStringStart)
+$endIndex = $content.IndexOf($searchStringEnd, $startIndex + $searchStringStart.Length)
+
+if ($startIndex -ge 0 -and $endIndex -ge 0){
+$before = $content.Substring(0, $startIndex)
+$after = $content.Substring($endIndex + $searchStringEnd.Length)
+$content = $before + $replacementString + $after
+Set-Content $file.FullName -Value $content
+}
+}
