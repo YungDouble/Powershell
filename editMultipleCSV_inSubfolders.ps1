b076fbd03d45fa2a83aec14cb540cb67f3dda398
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2023-07-28 16:06:53 -0500

    Update editMultipleCSV_inSubfolders.ps1
    added new items for AR stalled batches

--- editMultipleCSV_inSubfolders.ps1
+++ editMultipleCSV_inSubfolders.ps1
@@ -12,6 +12,10 @@ Get-ChildItem -Path $rootPath -Recurse -Include *.csv | ForEach-Object {
                 $_.DocCategory = "Miscellaneous"
                 $_.DocType = "Miscellaneous"
                 $_.DateofBirth = "1/1/1753"
+                $_.FirstName = "Admin"
+                $_.LastName = "SBD"
+                $_.IdentificationNumber = "GSB-AR-123"
+                
     }
 
     # Export the modified CSV data to the same file
