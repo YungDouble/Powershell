5b193abd0fc8fb34c4e6f6910619089d6ee77106
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2023-08-04 13:10:49 -0500

    Create CombineCSV.ps1
    Initial Commit

---
+++ CombineCSV.ps1
@@ -0,0 +1,21 @@
+# Set the import folder path
+$sourceFolder = "C:\Users\ddehoyos\Desktop\BatchesWorkbench"
+
+# Set the ouput file path
+$outputFile = "C:\Users\ddehoyos\Desktop\BatchesWorkbench\Cobmined.csv"
+
+# Get all CSV files in the source folder
+$csvFiles = Get-ChildItem $sourceFolder -Filter "*.csv"
+
+# Initialize an empty array to hold the data
+$combineData = @()
+
+# Loop through each CSV and append its data to the Combined CSV
+foreach ($csvFile in $csvFiles) {
+	$data = Import-Csv $csvFile.FullName
+	$combinedData += $data
+	
+}
+
+# Export the combined Csv
+$combineData | Export-Csv -Path $outputFile -NoTypeInformation
