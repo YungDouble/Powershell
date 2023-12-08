23a2e8d22624d3614d66c26ae3377350ceb28b18
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2023-12-08 15:10:58 -0600

    Update moveBatches.ps1
    intial commit

--- moveBatches.ps1
+++ moveBatches.ps1
@@ -1,37 +1,35 @@
+# This script is to move a list of batches from one location to a new 
 
+# Set the source and destination folder paths
+$sourceFolder = "./"
+$destinationFolder = "./DestinationFolder"
 
-$source = Get-Location
-$destination = Join-Path -Path $source -ChildPath "NewLocation"
-
-if (-not (Test-Path -Path $destination -PathType Container)) {
-    New-Item -Path $destination -ItemType Directory
+# Ensure the destination folder exists
+if (-not (Test-Path -Path $destinationFolder -PathType Container)) {
+    New-Item -Path $destinationFolder -ItemType Directory -Force
 }
 
-$csvFilePath = "files.csv"
+# Specify the path to the CSV file
+$csvFilePath = ".\files.csv"
 
-$csvData = Import-Csv -Path $csvFilePath
+# Read the CSV file
+$folderList = Import-Csv -Path $csvFilePath
 
-foreach ($row in $csvData) {
-    # Get the folder name from the CSV
-    $folderName = $row.FolderName
+# Iterate through each folder in the CSV and move it
+foreach ($folder in $folderList) {
+    $folderName = $folder.FolderName
 
-    $sourcePath = Join-Path -Path $source -ChildPath $folderName
-    $destinationPath = Join-Path -Path $destination -ChildPath $folderName
+    $sourcePath = Join-Path -Path $sourceFolder -ChildPath $folderName
+    $destinationPath = Join-Path -Path $destinationFolder -ChildPath $folderName
 
+    # Check if the source folder exists
     if (Test-Path -Path $sourcePath -PathType Container) {
-        # Move the entire folder to the destination
+        # Move the folder to the destination
         Move-Item -Path $sourcePath -Destination $destinationPath -Force
         Write-Host "Moved folder '$folderName' to '$destinationPath'"
-    }
-    elseif (Test-Path -Path $sourcePath -PathType Leaf) {
-        # Move a single file to the destination
-        Move-Item -Path $sourcePath -Destination $destinationPath -Force
-        Write-Host "Moved file '$folderName' to '$destinationPath'"
-    }
-    else {
-        Write-Host "Folder or file '$folderName' does not exist"
+    } else {
+        Write-Host "Source folder '$folderName' does not exist"
     }
 }
 
-# Output a message when the script is finished
 Write-Host "Script completed."
