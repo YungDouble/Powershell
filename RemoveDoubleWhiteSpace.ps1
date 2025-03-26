d416882ecb1a6fc0765f0e24e2b2b20ee7fffee6
Davos DeHoyos <44310581+YungDouble@users.noreply.github.com>
2025-03-26 16:32:46 -0500

    Update RemoveDoubleWhiteSpace.ps1
    added another function to compare

--- RemoveDoubleWhiteSpace.ps1
+++ RemoveDoubleWhiteSpace.ps1
@@ -12,3 +12,16 @@ Get-ChildItem -Recurse -File | Where-Object { $_.Name -match "  " } | ForEach-Ob
         Write-Warning "Skipped (file already exists): $newName"
     }
 }
+
+
+Get-ChildItem -Recurse -Filter *.pdf | Where-Object { $_.Name -match "\s{2,}" } | ForEach-Object {
+    $newName = ($_.Name -replace '\s{2,}', ' ')  # Replace double or more spaces with a single space
+    $newPath = Join-Path -Path $_.DirectoryName -ChildPath $newName
+
+    if (-not (Test-Path $newPath)) {
+        Rename-Item -Path $_.FullName -NewName $newName
+        Write-Host "Renamed:`n$($_.FullName)`n→ $newName`n"
+    } else {
+        Write-Warning "Skipped (target file already exists): $newName"
+    }
+}
